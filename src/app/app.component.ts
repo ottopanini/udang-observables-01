59d87851b3e452a82ade2621c604e17f80bf047e
Christian Preß <Christian.Press@syracos.com>
2020-05-23 15:48:46 +0200

    Subjects - use Subject over EventEmitter -> recommended for always in the future (Except on @Output -> only for Service emitters though)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from './user.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSubscription: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(didActivate => {
+    this.activatedSubscription = this.userService.activatedEmitter.subscribe(didActivate => {
       this.userActivated = didActivate;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSubscription.unsubscribe();
   }
 }
